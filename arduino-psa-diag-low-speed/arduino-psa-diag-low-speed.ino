f2a99d93d7222944daef046f6102e1c4f8340922
Ludwig <ludwig@v>
2020-11-02 10:17:23 +0100

    Copyright notice

--- arduino-psa-diag-low-speed/arduino-psa-diag-low-speed.ino
+++ arduino-psa-diag-low-speed/arduino-psa-diag-low-speed.ino
@@ -1,3 +1,21 @@
+/*
+Copyright 2020, Ludwig V. <https://github.com/ludwig-v>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License at <http://www.gnu.org/licenses/> for
+more details.
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+*/
+
 /////////////////////
 //    Libraries    //
 /////////////////////
